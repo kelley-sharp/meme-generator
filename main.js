44da510a42f999a8c1c40377b116e83d07c70b71
kelley <kelleylsharp@gmail.com>
2018-05-27 13:13:05 -0700

    cleaned-up code

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-  console.log('main function');
   var form = document.getElementById('my_form');
   form.addEventListener('submit', function(event) {
     event.preventDefault();
-    var picURL = event.target.picture.value; // event -> { target: { picture: { value: 'http://somethingtheuserentered.com' }}}
+    var picURL = event.target.picture.value;
     var topCaption = event.target.topCaption.value;
     var bottomCaption = event.target.bottomCaption.value;
     generateMeme(picURL, topCaption, bottomCaption);
@@ -14,43 +13,45 @@ function main() {
 }
 
 function generateMeme(imageURL, topCaptionText, bottomCaptionText) {
+  //div to hold meme image
   var imageHolder = document.createElement('div');
-
   imageHolder.style.height = '400px';
   imageHolder.style.position = 'relative';
   document.getElementById('container').appendChild(imageHolder);
   imageHolder.style.backgroundImage = `url${imageURL}`;
   imageHolder.className = 'image_holder';
 
+  //x button to remove the div that holds meme image
   var x = document.createElement('button');
   x.innerText = '✘';
   x.className = 'x';
+  imageHolder.appendChild(x);
   x.addEventListener('click', function() {
     container.removeChild(imageHolder);
   });
-  imageHolder.appendChild(x);
 
+  //creation of meme image from URL
   var memeImg = document.createElement('img');
   memeImg.className = 'meme';
   memeImg.style.maxHeight = '400px';
   memeImg.setAttribute('src', imageURL);
 
+  //add top caption
   var memeTopTxt = document.createElement('span');
   memeTopTxt.innerText = topCaptionText;
   memeTopTxt.setAttribute('id', 'top_text');
 
+  //add bottom caption
   var memeBotTxt = document.createElement('span');
   memeBotTxt.innerText = bottomCaptionText;
   memeBotTxt.setAttribute('id', 'bot_text');
 
+  //attach everything to the div that holds meme image
   imageHolder.appendChild(memeTopTxt);
   imageHolder.appendChild(memeImg);
   imageHolder.appendChild(memeBotTxt);
 
+  //create container to hold imageHolder for positioning purposes
   var container = document.getElementById('container');
   container.appendChild(imageHolder);
-}
-
-function resetForm(inputs) {
-  inputs.reset();
 }
